7dafdd24187a54740603d6a8d8820a9d4327e2d4
KTJHouston <ktjhouston@gmail.com>
2018-10-17 15:39:18 -0500

    Added more training sets

--- Neural_Net_Game_Wrapper.py
+++ Neural_Net_Game_Wrapper.py
@@ -55,6 +55,29 @@ def create_addition(input_num, output_num):
 	out[total] = 1.
 	return inp, out
 
+def create_addition_2(output_num):
+	inp = []
+	out = []
+	total = randint(0, output_num-1)
+	for o in range(0, output_num):
+		out.append(0.)
+	out[total] = 1.
+	inp.append(randint(0, total))
+	inp.append(total - inp[0])
+	return inp, out
+
+def create_AND(r):
+	inp = [[0, 0], [0, 1], [1, 0], [1, 1], [1, 1]]
+	out = [[0, 1], [0, 1], [0, 1], [1, 0], [1, 0]]
+	inp = inp[r]
+	out = out[r]
+	return inp, out
+
+def pp(flist):
+	print('[', end='')
+	for f in range(len(flist)-1):
+		print(f'%.2f, ' % flist[f], end='')
+	print(f'%.2f]' % flist[len(flist)-1])
 
 
 
@@ -65,8 +88,10 @@ def create_addition(input_num, output_num):
 
 
 
-layers = [2, 20, 10]
-learning_rate = 0.001
+
+
+layers = [2, 6, 5]
+learning_rate = 0.01
 NN = Neural_Net(layers, learning_rate)
 '''
 x, d = create_addition(layers[0], layers[len(layers)-1])
@@ -77,33 +102,42 @@ print(c)
 print()
 '''
 
+
 test_size = 1 #Should be odd
 total_wrong = []
 wrong = []
-for i in range(10000):
-	x, d = create_addition(layers[0], layers[len(layers)-1])
-	c = NN.train(x)
+for i in range(1000000):
+	x, d = create_addition_2(layers[len(layers)-1])
+	c, conf = NN.train(x)
 	if c != d : 
 		total_wrong.append(i)
 		wrong.append(i)
 	if i % test_size == 0 :
-		if len(wrong) <= (test_size // 2):
-			NN.reward(True)
+		#if True:
+		#if len(wrong) <= (test_size // 2):
+		if len(wrong) == 0:
+			NN.reward(True, 1.)
 		else:
-			NN.reward(False)
+			NN.reward(False, .01)
+			#NN.reward_clear()
 		wrong = []
 
-x, d = create_addition(layers[0], layers[len(layers)-1])
-print(x)
-print(d)
-p = NN.predict(x)
-print(p)
-c = NN.collapse(p)
-print(c)
-if c == d :
-	print('CORRECT')
-else:
-	print('WRONG')
+for i in range(10):
+	x, d = create_addition_2(layers[len(layers)-1])
+	print(x)
+	#print(d)
+	p = NN.predict(x)
+	print(p)
+	c, confidence = NN.collapse(p)
+	print(c)
+	print(d)
+	print('Confidence:')
+	pp(confidence)
+	if c == d :
+		print('CORRECT')
+	else:
+		print('WRONG')
+	print()
 
 '''
 x = [x[1], x[0]]
@@ -117,7 +151,7 @@ if c == d :
 	print('CORRECT')
 else:
 	print('WRONG')
-'''
 
 print()
 print(NN)
+'''
